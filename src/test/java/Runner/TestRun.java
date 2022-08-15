8616406af8eac04c212d5521ec11279a8a30f07b
Ahmedbendhaya <102963247+Ahmedbendhaya@users.noreply.github.com>
2022-08-15 17:08:15 +0100

    Update TestRun.java

--- src/test/java/Runner/TestRun.java
+++ src/test/java/Runner/TestRun.java
@@ -1,10 +1,11 @@
 package Runner;
 
 import org.junit.runner.RunWith;
-
+import org.testng.annotations.Test;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+@Test
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		
